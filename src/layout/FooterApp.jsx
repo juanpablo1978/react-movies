c523e1de1cc9b7782f21de0c436db4451ffd1ca8
juanpablo1978 <jpandrujovich@gmail.com>
2023-03-02 16:44:52 -0300

    footer

--- src/layout/FooterApp.jsx
+++ src/layout/FooterApp.jsx
@@ -9,21 +9,29 @@ const FooterApp = () => {
     <section className=' bg-slate-800 h-[120px] w-full'>
       <article>
       <div className='flex items-center justify-center text-white font-semibold py-5'>
-      Juan Pablo Andrujovich
+      <p>Juan Pablo Andrujovich 2023</p>
       </div>
       <div className='flex items-center justify-center text-white gap-3 text-lg'>
-        <button>
+        <a href="https://github.com/juanpablo1978" target="_blank">
+        <button> 
           <BsGithub/>
         </button>
+        </a>
+        <a href="https://www.linkedin.com/in/juan-pablo-andrujovich-921386189/" target="_blank">
         <button>
           <BsLinkedin/>
         </button>
+        </a>
+        <a href="https://www.instagram.com/jp.andrujovich/" target="_blank">
         <button>
           <BsInstagram/>
         </button>
+        </a>
+        <a href="https://www.facebook.com/juanpablo.andrujovich" target="_blank">
         <button>
           <BsFacebook/>
         </button>
+        </a>
         </div>
         </article>
       </section>
